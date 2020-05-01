24c8d72e842f43c038c7dfe19e3e824f5be5e4d7
Zoltan Magyari <zoltan.magyari@msg.group>
2020-05-01 08:42:13 +0300

    java trainings

--- openssl/decrypt.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-REM source: https://www.shellhacks.com/encrypt-decrypt-file-password-openssl/
-
-
-set fileName=%1%
-set fileName=%fileName:.enc=%
-echo %fileName%
-
-openssl enc -d -aes-256-cbc -pbkdf2 -a -in %filename%.enc -out %filename%
--- openssl/encrypt.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-REM https://www.shellhacks.com/encrypt-decrypt-file-password-openssl/
-
-openssl enc -aes-256-cbc -salt -pbkdf2 -a -in %1 -out %1.enc
