67320aab91b88faaa24322f3c2b8f2d9b106c929
Magyari Zoltán <mz.zoltan.magyari@gmail.com>
2021-07-07 20:54:43 +0300

    PluralSight: Building Applications with React and Redux

--- react/Building Applications with React and Redux/excersize/src/redux/actions/courseActions.js
+++ react/Building Applications with React and Redux/excersize/src/redux/actions/courseActions.js
@@ -1,12 +1,16 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
-}
-
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function updateCourseSuccess(courses) {
+  return { type: types.UPDATE_COURSE_SUCCESS, courses };
+}
+
+export function createCourseSuccess(courses) {
+  return { type: types.CREATE_COURSE_SUCCESS, courses };
 }
 
 export function loadCourses() {
@@ -15,6 +19,21 @@ export function loadCourses() {
       .getCourses()
       .then((courses) => {
         dispatch(loadCoursesSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch) {
+    return courseApi
+      .saveCourse()
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
       })
       .catch((error) => {
         throw error;
