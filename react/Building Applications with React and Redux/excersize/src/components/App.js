543910e15c17832ba8d56107544c144be7b27fdb
Magyari Zoltán <mz.zoltan.magyari@gmail.com>
2021-07-13 11:29:56 +0300

    PluralSight: Building Applications with React and Redux - remove react version

--- react/Building Applications with React and Redux/excersize/src/components/App.js
+++ react/Building Applications with React and Redux/excersize/src/components/App.js
@@ -12,7 +12,6 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <div className="container-fluid">
-      <h1>{React.version}</h1>
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
