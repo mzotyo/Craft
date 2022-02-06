d74eb551b14821db879d9f759db330373f1bf17f
Magyari Zoltan <zoltan.magyari@msg.group>
2022-02-06 05:14:00 -0500

    Decorator Design Pattern
    unit test has been simplified

--- design patterns/decorator/decorator.test.ts
+++ design patterns/decorator/decorator.test.ts
@@ -1,16 +1,17 @@
 import { Component, ConcreteComponent, ConcreteDecoratorA, ConcreteDecoratorB } from './decorator';
 
+console.log = jest.fn();
+
+/**
+ * The client code works with all objects using the Component interface. This 
+ * way it can stay independent of the concrete classes of components it works 
+ * with.
+ */
+function clientCode(component: Component) {
+    console.log(`RESULT: ${component.operation()}`);
+}
+
 describe('Example usage of decorator pattern', () => {
-
-    /**
-     * The client code works with all objects using the Component interface. This 
-     * way it can stay independent of the concrete classes of components it works 
-     * with.
-     */
-    function clientCode(component: Component) {
-        console.log(`RESULT: ${component.operation()}`);
-    }
-
     /**
      * This way the client code can siupport both simple components...
      */
@@ -25,24 +26,28 @@ describe('Example usage of decorator pattern', () => {
     const decorator1 = new ConcreteDecoratorA(simple);
     const decorator2 = new ConcreteDecoratorB(decorator1);
 
-    const mockLog = jest.fn();
-    console.log = mockLog;
     beforeEach(() => {
         mockLog.mockClear();
     });
 
     test('Simple component', () => {
         clientCode(simple);
-        expect(console.log).toHaveBeenCalledWith('RESULT: ConcreteComponent');
+        
+        expect(console.log)
+            .toHaveBeenCalledWith('RESULT: ConcreteComponent');
     });
     
     test('Decorated with Decorator1', () => {
         clientCode(decorator1);
-        expect(console.log).toHaveBeenCalledWith('RESULT: ConcreteDecoratorA( ConcreteComponent )');
+        
+        expect(console.log)
+            .toHaveBeenCalledWith('RESULT: ConcreteDecoratorA( ConcreteComponent )');
     });
 
     test('Decorated with Decorator2', () => {
         clientCode(decorator2);
-        expect(console.log).toHaveBeenCalledWith('RESULT: ConcreteDecoratorB( ConcreteDecoratorA( ConcreteComponent ) )');
+        
+        expect(console.log)
+            .toHaveBeenCalledWith('RESULT: ConcreteDecoratorB( ConcreteDecoratorA( ConcreteComponent ) )');
     });
 });
