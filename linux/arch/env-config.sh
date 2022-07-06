ee56431a50687f179b683414c170a8c1ef2aa1c4
John Doe <asghtr@freemail.com>
2022-07-06 21:30:35 +0300

    Arch linux evironment configuration
    Installing dwm from suckless.org

--- linux/arch/env-config.sh
+++ linux/arch/env-config.sh
@@ -7,7 +7,7 @@ pacman -S vim
 pacman -S  openssh
 
 # systemctl status sshd
-systemctl start sshd
+# systemctl start sshd
 systemctl enable sshd
 
 # port forward from Vritualox
@@ -72,6 +72,10 @@ pacman -S base-devel cmake make unzip ninja tree-sitter curl
 mkdir ~/Tools
 
 # -----------------------------------------------------------------------------
+# Wget downloader
+pacamn -S wget
+
+# -----------------------------------------------------------------------------
 # Neovim configuration
 pacman -S neovim
 
@@ -91,6 +95,53 @@ pacman -S python-pip
 
 pip install pynvim
 npm install -g neovim
+
+# -----------------------------------------------------------------------------
+# Dwm window manager
+pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot
+
+# Rendering background
+pacman -S nitrogen
+
+# Window compositor: transparency, transition animations, shadows ...
+pacman -S picom
+
+# I don't know what is it for but it's needed to start dwm. At least in virtual-box
+pacman -S webkit2gtk
+
+# Config graphical ui
+cp /etc/X11/xinit/xinitrc .xinitrc
+
+# vim .xinitrc
+```shell
+# Keyboard Layout
+setxkbmap hu -model "pc101" -variant "101_qwerty_comma_dead"  &
+
+# Display Resolution
+xrandr --output Virtual-1 --mode 1920x1080 &
+
+# Compositor
+picom -f &
+
+# Execute DWM
+exec dwm
+```
+# Install dwm from suckless
+git clone https://git.suckless.org/dwm
+cd dwn
+make clean install
+# make unsinstall
+
+# Install siple terminal from suckless
+git clone https://git.suckless.org/st
+cd st
+make clean install
+# make uninstall
+
+# Starting the graphival user interface
+startx
+
+wget https://dl.suckless.org/st/st-0.8.tar.gz
 
 # -----------------------------------------------------------------------------
 reboot
